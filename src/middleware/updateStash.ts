b8576f9d1fbbf907b9c1d95051409571d572d614
apanicek <apanicek@pixelfederation.com>
2018-12-19 15:09:33 +0100

    Implementing stash updating middleware

--- src/middleware/updateStash.ts
+++ src/middleware/updateStash.ts
@@ -1,21 +1,26 @@
-import {responseFailed, response} from "../application";
-import {updateStash as model} from "../stash/update_stash";
+import {update} from "../stash/stash";
 import {BaseUser} from "../user/model";
+import {BadRequest} from "../application";
 
 export const updateStash = async (req, next) => {
     const response = await next(req);
 
-    if(!req.originalRequest || !req.originalRequest.stash) {
+    if(!response.originalRequest || !response.originalRequest.body.stash) {
         return response;
     }
 
-    const {previousVersionToken, data} = req.originalRequest.stash;
-
+    const {previousVersionToken, data} = response.originalRequest.body.stash;
     const user: BaseUser = {
-        identificator: req.originalRequest.user
+        identificator: response.originalRequest.user
     }
 
-    response.stash = await model(user, previousVersionToken, data);
+    try {
+        response.stash = await update(user, previousVersionToken, data);
+    } catch (e) {
+        if (e.code === 'ConditionalCheckFailedException') {
+            throw new BadRequest(`Previous token doesn't match`);
+        }
+    }
 
     return {...response};
 };
